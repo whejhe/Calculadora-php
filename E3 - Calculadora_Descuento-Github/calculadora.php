b42bd754ee88de58c71643438ca1333d1dc8ba78
Carlos <dominguezalacid@gmail.com>
2024-10-29 10:54:27 +0100

    actualizar

--- E3 - Calculadora_Descuento-Github/calculadora.php
+++ E3 - Calculadora_Descuento-Github/calculadora.php
@@ -5,7 +5,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="css/style.css">
-    <title>Document</title>
+    <title>Calculadora Descuento (Trabajo Colaborativo)</title>
 </head>
 
 <body>
